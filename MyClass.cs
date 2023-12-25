9e8897a1b97bb7c7041763b571fc940182dd5508
olimov Amir <olimov.amir@mail.ru>
2023-12-25 10:34:49 +0300

    Добавьте файлы проекта.

---
+++ MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Constructor
+{
+    internal class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("Конструктор");
+        }
+
+        static MyClass()
+        {
+            Console.WriteLine("Статистический конструктор");
+        }
+
+        public static void Foo() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Static_Constructor.MyClass myClass = new Static_Constructor.MyClass();
+        new Static_Constructor.MyClass();
+        new Static_Constructor.MyClass();
+
+    }
+}
